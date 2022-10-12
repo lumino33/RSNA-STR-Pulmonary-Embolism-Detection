dc44aef87ff1444586033564b9909a527ed2d54e
lumino33 <ledinhhung332000@gmail.com>
2022-10-12 12:42:55 +0700

    add focal loss

---
+++ src/focalloss.py
@@ -0,0 +1,23 @@
+
+import torch, sys, os, pdb
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FocalLoss(nn.Module):
+
+    def __init__(self, gamma = 1.0, device = "cpu"):
+        super(FocalLoss, self).__init__()
+        self.gamma = torch.tensor(gamma, dtype = torch.float32).to(device)
+        self.eps = 1e-6
+
+    def forward(self, input, target):
+        # input are not the probabilities, they are just the cnn out vector
+        # input and target shape: (bs, n_classes)
+        # sigmoid
+        probs = torch.sigmoid(input)
+        log_probs = -torch.log(probs)
+
+        focal_loss = torch.sum(  torch.pow(1-probs + self.eps, self.gamma).mul(log_probs).mul(target)  , dim=1)
+        # bce_loss = torch.sum(log_probs.mul(target), dim = 1)
+        
+        return focal_loss.mean() #, bce_loss
