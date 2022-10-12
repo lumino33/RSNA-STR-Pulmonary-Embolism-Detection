d127c42be703f313e0737ffd4030358332cee89a
lumino33 <ledinhhung332000@gmail.com>
2022-10-12 12:41:03 +0700

    change models

--- src/model.py
+++ src/model.py
@@ -1,23 +1,38 @@
-from turtle import forward
 import torch
 import torch.nn as nn
-import torchvision
-from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
+import torch.nn.functional as F
+from torch.cuda.amp import autocast
+import timm
 
-class EfficientNetB0(nn.Module):
-    def __init__(self):
-        super(EfficientNetB0, self).__init__()
-        self.model = efficientnet_b0(weights = EfficientNet_B0_Weights.DEFAULT)
-        self.model.classifier = nn.Sequential(nn.Linear(1280, 512), 
-                                 nn.ReLU(True),
-                                 nn.Dropout(0.3),
-                                 nn.Linear(512, 64),
-                                 nn.ReLU(True),
-                                 nn.Dropout(0.3),
-                                 nn.Linear(64,1),
-                                 nn.Sigmoid()
-                                 )
+
+class EfficientNet(nn.Module):
+    def __init__(self, backbone, pretrained=False, num_classes=1, channel=3, in_features=1280):
+        super(EfficientNet, self).__init__()
+        self.base =  timm.create_model(backbone, pretrained=pretrained,
+                                       num_classes = 0,
+                                       in_chans=channel)
+        self.fc1= nn.Linear(in_features, 2048, bias=True)
+        self.fc2= nn.Linear(2048, 1024, bias=True)
+        self.last_linear= nn.Linear(1024, num_classes, bias=True)
+    
+    @autocast()
     def forward(self, x):
-        x = self.model(x)
+        x = self.base(x)
+        x= self.fc1(x)
+        x= F.relu(x)
+        x= F.dropout(x, p=0.5, training=self.training)
+        x= self.fc2(x)
+        x= F.relu(x)
+        x= F.dropout(x, p=0.3, training=self.training)
+        x= self.last_linear(x)
         return x
-        
+
+    @autocast()
+    def extract_features(self, x):
+        x= self.base(x)
+        x= self.fc1(x)
+        x= F.relu(x)
+        emb= self.fc2(x)
+        out= F.relu(emb)
+        out= self.last_linear(out)
+        return emb, out
